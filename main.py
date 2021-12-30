434e4816ffa96960fb0f40b6fce251f8e79a0a36
Michael Schmitt <schmiddim@gmx.at>
2021-12-30 21:55:42 +0100

    main done

---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+import requests as r
+from modules.releases import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', dest='url', help="url to github repo", required=True)
+    parser.add_argument('--mode',
+                        dest='mode',
+                        help="get latest <asset_url> || get latest <release_tag>",
+                        required=True,
+                        choices=["asset_url", "release_tag"]
+                        )
+
+    args = parser.parse_args()
+    api_url = get_api_url_for_repo(args.url)
+    data = r.get(api_url).json()
+
+    if args.mode == "asset_url":
+        print(get_asset_download_url_by_pattern(data, ".*linux"))
+    if args.mode == "release_tag":
+        print(get_latest_tag(data))

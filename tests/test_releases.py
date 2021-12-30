b70bfe869c43c4922edae19458a792010804f905
Michael Schmitt <schmiddim@gmx.at>
2021-12-30 21:05:49 +0100

    test refactored

--- tests/test_releases.py
+++ tests/test_releases.py
@@ -13,7 +13,15 @@ class TestReleases:
         assert got == want
 
     def test_get_tag(self):
-        test_table = [
+        for test in TestReleases.get_test_table():
+            data = TestReleases.load_json_fixture(test.get("path"))
+            want = test.get("tag")
+            got = get_latest_tag(data)
+            assert got == want
+
+    @staticmethod
+    def get_test_table():
+        return [
 
             {"path": "tests/fixtures/releases-wget-unzip.json",
              "tag": "v2"
@@ -24,11 +32,6 @@ class TestReleases:
 
              }
         ]
-        for test in test_table:
-            data = TestReleases.load_json_fixture(test.get("path"))
-            want = test.get("tag")
-            got = get_latest_tag(data)
-            assert got == want
 
     @staticmethod
     def load_json_fixture(path):

5a9c8f9cf10dda64da1460d568f499c64523557f
Michael Schmitt <schmiddim@gmx.at>
2021-12-30 23:24:56 +0100

    fucking output stuff

--- main.py
+++ main.py
@@ -5,27 +5,12 @@ from modules.releases import *
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', dest='url', help="url to github repo", required=True)
-    parser.add_argument('--pattern', dest='pattern', help="pattern for download url", required=False)
-    parser.add_argument('--mode',
-                        dest='mode',
-                        help="get latest <asset_url> || get latest <release_tag>",
-                        required=True,
-                        choices=["asset_url", "release_tag"]
-                        )
-
+    parser.add_argument('--pattern', dest='pattern', help="pattern for download url", required=False, default=".*")
     args = parser.parse_args()
+
     api_url = get_api_url_for_repo(args.url)
     data = r.get(api_url).json()
     url = get_asset_download_url_by_pattern(data, args.pattern)
     tag = get_latest_tag(data)
-    if args.mode == "asset_url":
-        if args.pattern is None:
-            print("--pattern must be passed when mode is asset_url")
-            exit(1)
-
-        print(url)
-        print(f"::set-output name=release_url::{url}")
-        print(f"::set-output name=release_tag::{tag}")
-
-    if args.mode == "release_tag":
-        print(get_latest_tag(data))
+    print(f"::set-output name=release_url::{url}")
+    print(f"::set-output name=release_tag::{tag}")
